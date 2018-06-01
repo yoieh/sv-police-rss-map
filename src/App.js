4983989021674b5bebbfa4486724b88d6bed3d57
yoieh <yoieh@live.se>
2018-06-01 18:27:58 +0200

    remove old map

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 
-// import GoogleApiWrapper from "./GoogleApiWrapper";
 import Map from "./Map";
 
 class App extends Component {
