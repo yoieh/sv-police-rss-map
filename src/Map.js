84f5cf4c4ae0664fe395f8fb9a3adced3a9fd712
yoieh <yoieh@live.se>
2018-06-02 04:07:44 +0200

    added filter options !

--- src/Map.js
+++ src/Map.js
@@ -9,34 +9,32 @@ import {
   InfoWindow
 } from "react-google-maps";
 
+import SimpleLineIcon from "react-simple-line-icons";
+
 import { policeFeed } from "./services/police-rss";
 import cities from "./services/cities.json";
 import icons from "./services/icons";
 import filterPlaces from "./services/filterPlaces";
+import MapControl from "./MapControl";
 
 class MapComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
-      incidents: null
+      shownCollapse: [],
+      hiddenIcons: []
     };
   }
 
   componentWillMount() {
     policeFeed()
       .then(items => items.map(i => filterPlaces(cities, i)))
-      .then(data => data.map(i => ({ ...i, icon: icons[i.incident] })))
+      .then(data => icons(data))
+
       .then(data => {
         this.setState({
-          posts: icons(data)
-          // incidents: data.reduce((results, item) => {
-          //   let incident = item.title.split(", ").slice(1)[0];
-          //   results[incident] = results[incident]
-          //     ? { ...results[incident], amount: results[incident].amount + 1 }
-          //     : { amount: 1, icon: icons[incident] };
-          //   return results;
-          // }, {})
+          posts: data
         });
       });
   }
@@ -52,44 +50,257 @@ class MapComponent extends Component {
     // });
   };
 
+  toggleCollapse = key => {
+    let shownCollapse = this.state.shownCollapse;
+
+    if (shownCollapse.some(i => i === key)) {
+      shownCollapse = shownCollapse.filter(i => i !== key);
+    } else {
+      shownCollapse.push(key);
+    }
+    this.setState({
+      shownCollapse: shownCollapse
+    });
+  };
+
+  filerIncident = name => {
+    this.setState({
+      posts: this.state.posts.map(
+        post =>
+          name === post.incident ? { ...post, hidden: !post.hidden } : post
+      )
+    });
+  };
+
+  filerIcon = id => {
+    let hiddenIcons = this.state.hiddenIcons;
+
+    if (hiddenIcons.some(i => i === id)) {
+      hiddenIcons = hiddenIcons.filter(i => i !== id);
+    } else {
+      hiddenIcons.push(id);
+    }
+
+    console.log(hiddenIcons);
+
+    this.setState({
+      hiddenIcons: hiddenIcons
+    });
+  };
+
   render() {
     return (
       <GoogleMap
         defaultZoom={5}
         defaultCenter={new google.maps.LatLng(59.334591, 18.06324)}
-        defaultOptions={{ maxZoom: 10, streetViewControl: false, mapTypeControl: false }}
+        defaultOptions={{
+          maxZoom: 10,
+          streetViewControl: false,
+          mapTypeControl: false
+        }}
       >
-        {this.state.posts.map((post, key) => {
-          return (
-            <Marker
-              key={key}
-              title={post.incident}
-              //name={key}
-              onClick={() => this.togglePost(key)}
-              icon={{
-                url: post.icon,
-                anchor: new google.maps.Point(12, 12),
-                scaledSize: new google.maps.Size(24, 24)
+        {this.state.posts
+          ? this.state.posts.map((post, key) => {
+              return !post.hidden &&
+                !this.state.hiddenIcons.some(i => post.icon.id === i) ? (
+                <Marker
+                  key={key}
+                  title={post.incident}
+                  onClick={() => this.togglePost(key)}
+                  icon={{
+                    url: post.icon.uri,
+                    anchor: new google.maps.Point(12, 12),
+                    scaledSize: new google.maps.Size(24, 24)
+                  }}
+                  position={
+                    new google.maps.LatLng(
+                      post.center.latitude,
+                      post.center.longitude
+                    )
+                  }
+                >
+                  {key === this.state.selected ? (
+                    <InfoWindow onCloseClick={() => this.togglePost(key)}>
+                      <div>
+                        <h2>{post.title}</h2>
+                        <p>{post.content}</p>
+                        <a href={post.link}>Go to Post</a>
+                      </div>
+                    </InfoWindow>
+                  ) : null}
+                </Marker>
+              ) : null;
+            })
+          : null}
+        <MapControl position={google.maps.ControlPosition.TOP_LEFT}>
+          <div style={{ margin: "10px 0" }}>
+            <button
+              onClick={() => this.setState({ show: !this.state.show })}
+              style={{
+                padding: "5px",
+                width: "100%",
+                borderRadius: "5px",
+                backgroundColor: "#fff"
               }}
-              position={
-                new google.maps.LatLng(
-                  post.center.latitude,
-                  post.center.longitude
-                )
-              }
             >
-              {key === this.state.selected ? (
-                <InfoWindow onCloseClick={() => this.togglePost(key)}>
-                  <div>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                    <a href={post.link}>Go to Post</a>
-                  </div>
-                </InfoWindow>
-              ) : null}
-            </Marker>
-          );
-        })}
+              {this.state.show ? (
+                <span>
+                  <SimpleLineIcon
+                    name="layers
+"
+                  />
+                  {"Hide filters"}
+                </span>
+              ) : (
+                <span>
+                  <SimpleLineIcon
+                    name="layers
+"
+                  />
+                  {"Show filters"}
+                </span>
+              )}
+            </button>
+            <div
+              style={{
+                overflow: "auto",
+                maxHeight: "500px"
+              }}
+            >
+              {this.state.show
+                ? this.state.posts
+                    .reduce((results, item, key) => {
+                      let incident = item.title.split(", ").slice(1)[0];
+                      let found = results.findIndex(i => i.name === incident);
+                      found > -1
+                        ? results[found].amount++
+                        : results.push({
+                            ...item,
+                            amount: 1,
+                            name: incident,
+                            icon: item.icon.uri,
+                            iconId: item.icon.id,
+                            incidentId: key
+                          });
+                      return results;
+                    }, [])
+                    .sort(function(a, b) {
+                      if (a.name < b.name) return -1;
+                      if (a.name > b.name) return 1;
+                      return 0;
+                    })
+                    .reduce((results, item) => {
+                      let found = results.findIndex(i =>
+                        i.find(i => i.icon === item.icon)
+                      );
+                      found > -1
+                        ? results[found].push(item)
+                        : results.push([item]);
+                      return results;
+                    }, [])
+                    .map((i, key) => (
+                      <div
+                        key={key}
+                        style={{
+                          backgroundColor: "#fff"
+                        }}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            background: "#c0c0c0",
+                            padding: "5px",
+                            paddingLeft: "10px",
+                            paddingRight: "10px",
+                            alignItems: "center"
+                          }}
+                        >
+                          <input
+                            type="checkbox"
+                            checked={
+                              !this.state.hiddenIcons.some(
+                                j => j === i[0].iconId
+                              )
+                            }
+                            onChange={() => this.filerIcon(i[0].iconId)}
+                          />
+                          <img
+                            width={20}
+                            height={20}
+                            src={i[0].icon}
+                            style={{
+                              marginLeft: "10px"
+                            }}
+                          />
+
+                          <span
+                            style={{
+                              marginLeft: "10px"
+                            }}
+                          >
+                            {i.reduce((result, j, key) => result + j.amount, 0)}
+                          </span>
+                          <span
+                            onClick={() => this.toggleCollapse(key)}
+                            style={{
+                              marginLeft: "10px"
+                            }}
+                          >
+                            {this.state.shownCollapse.some(i => i === key) ? (
+                              <SimpleLineIcon name="arrow-up" />
+                            ) : (
+                              <SimpleLineIcon name="arrow-down" />
+                            )}
+                          </span>
+                        </div>
+                        <div>
+                          {this.state.shownCollapse.some(i => i === key)
+                            ? i.map((j, key) => (
+                                <div
+                                  key={key}
+                                  style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    padding: "5px",
+                                    paddingRight: "10px",
+                                    alignItems: "center"
+                                  }}
+                                >
+                                  <input
+                                    type="checkbox"
+                                    checked={
+                                      !j.hidden &&
+                                      !this.state.hiddenIcons.some(
+                                        j => j === i[0].iconId
+                                      )
+                                    }
+                                    onChange={() => this.filerIncident(j.name)}
+                                  />
+                                  <span
+                                    style={{
+                                      marginLeft: "5px"
+                                    }}
+                                  >
+                                    {j.name}
+                                  </span>
+                                  <span
+                                    style={{
+                                      marginLeft: "10px"
+                                    }}
+                                  >
+                                    {j.amount}
+                                  </span>
+                                </div>
+                              ))
+                            : null}
+                        </div>
+                      </div>
+                    ))
+                : null}
+            </div>
+          </div>
+        </MapControl>
       </GoogleMap>
     );
   }
--- src/services/icons.js
+++ src/services/icons.js
@@ -1,80 +1,101 @@
 export const getIcon = params => {
+  const iconMap = [
+    { id: 0, uri: require("../icons/cross-2.png") },
+    { id: 1, uri: require("../icons/caution.png") },
+    { id: 2, uri: require("../icons/robbery.png") },
+    { id: 3, uri: require("../icons/fire.png") },
+    { id: 4, uri: require("../icons/boxing.png") },
+    { id: 5, uri: require("../icons/animal-shelter-export.png") },
+    { id: 6, uri: require("../icons/bar.png") },
+    { id: 7, uri: require("../icons/theft.png") },
+    { id: 8, uri: require("../icons/police.png") },
+    { id: 9, uri: require("../icons/crimescene.png") },
+    { id: 10, uri: require("../icons/car.png") },
+    { id: 11, uri: require("../icons/drugstore.png") },
+    { id: 12, uri: require("../icons/icy_road.png") },
+    { id: 13, uri: require("../icons/information.png") },
+    { id: 14, uri: require("../icons/shooting.png") },
+    { id: 15, uri: require("../icons/barrier.png") },
+    { id: 16, uri: require("../icons/caraccident.png") },
+    { id: 17, uri: require("../icons/symbol_inter.png") }
+  ];
+  
   switch (params) {
     case "Anträffad död":
-      return "../icons/cross-2";
+      return iconMap[0];
     case "Arbetsplatsolycka":
-      return require("../icons/caution.png");
+      return iconMap[1];
     case "Bedrägeri":
-      return require("../icons/robbery.png");
+      return iconMap[2];
     case "Brand":
-      return require("../icons/fire.png");
+      return iconMap[3];
     case "Bråk":
-      return require("../icons/boxing.png");
+      return iconMap[4];
     case "Djur skadat/omhändertaget":
-      return require("../icons/animal-shelter-export.png");
+      return iconMap[5];
     case "Farligt föremål":
-      return require("../icons/caution.png");
+      return iconMap[1];
     case "Fylleri/LOB":
-      return require("../icons/bar.png");
+      return iconMap[6];
     case "Inbrott":
-      return require("../icons/theft.png");
+      return iconMap[7];
     case "Kontroll person/fordon":
-      return require("../icons/police.png");
+      return iconMap[8];
     case "Larm Överfall":
-      return require("../icons/boxing.png");
+      return iconMap[3];
     case "Misshandel":
-      return require("../icons/boxing.png");
+      return iconMap[4];
     case "Mord/dråp":
-      return require("../icons/crimescene.png");
+      return iconMap[9];
     case "Motorfordon":
-      return require("../icons/car.png");
+      return iconMap[10];
     case "Narkotikabrott":
-      return require("../icons/drugstore.png");
+      return iconMap[11];
     case "Olovlig körning":
-      return require("../icons/icy_road.png");
+      return iconMap[12];
     case "Rattfylleri":
-      return require("../icons/bar.png");
+      return iconMap[6];
     case "Rån":
-      return require("../icons/robbery.png");
+      return iconMap[2];
     case "Rån väpnat":
-      return require("../icons/robbery.png");
+      return iconMap[2];
     case "Rån övrigt":
-      return require("../icons/robbery.png");
+      return iconMap[2];
     case "Sammanfattning kväll och natt":
-      return require("../icons/information.png");
+      return iconMap[13];
     case "Sammanfattning natt":
-      return require("../icons/information.png");
+      return iconMap[13];
     case "Sammanfattning vecka":
-      return require("../icons/information.png");
+      return iconMap[13];
     case "Sedlighetsbrott":
-      return require("../icons/caution.png");
+      return iconMap[1];
     case "Skadegörelse":
-      return require("../icons/caution.png");
+      return iconMap[1];
     case "Skottlossning":
-      return require("../icons/shooting.png");
+      return iconMap[14];
     case "Stöld":
-      return require("../icons/theft.png");
+      return iconMap[7];
     case "Stöld/inbrott":
-      return require("../icons/theft.png");
+      return iconMap[7];
     case "Trafikbrott":
-      return require("../icons/car.png");
+      return iconMap[10];
     case "Trafikhinder":
-      return require("../icons/barrier.png");
+      return iconMap[15];
     case "Trafikkontroll":
-      return require("../icons/police.png");
+      return iconMap[8];
     case "Trafikolycka":
-      return require("../icons/caraccident.png");
+      return iconMap[16];
     case "Uppdatering":
-      return require("../icons/information.png");
+      return iconMap[13];
     case "Övrigt":
-      return require("../icons/information.png");
+      return iconMap[13];
     default:
-      return require("../icons/symbol_inter.png");
+      return iconMap[17];
   }
 };
 
-export default data =>
-  data.map(item => {
+export default uri =>
+  uri.map(item => {
     let incident = item.title.split(", ").slice(1)[0];
     return {
       ...item,
